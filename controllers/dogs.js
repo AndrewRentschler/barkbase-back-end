e3a6fc81cb13802a9f7f2cb864811a45c4bf3790
Julian <julian.franco@icloud.com>
2023-05-15 17:04:35 -0400

    edit addPhoto function

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -106,16 +106,16 @@ async function deleteComment(req, res) {
 async function addPhoto(req, res) {
   try {
     const imageFile = req.files.photo.path
-    const profile = await Profile.findById(req.params.id)
+    const dog = await Dog.findById(req.params.id)
 
     const image = await cloudinary.uploader.upload(
       imageFile, 
       { tags: `${req.user.email}` }
     )
-    profile.photo = image.url
+    dog.photo = image.url
     
-    await profile.save()
-    res.status(201).json(profile.photo)
+    await dog.save()
+    res.status(201).json(dog.photo)
   } catch (err) {
     console.log(err)
     res.status(500).json(err)
